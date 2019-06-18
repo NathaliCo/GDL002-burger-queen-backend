fad8caa269f1676a9b8fb36b8f4853320a1530cd
unknown <nathaliecortz@hotmail.com>
2019-06-18 12:49:56 -0500

    Update schema, and err whit unique path and required path

---
+++ models/Product.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: [true, 'The name is required']
+    },
+    price: {
+        type: String,
+        required: [true, 'The price is required']
+    },
+});
+
+
+ProductSchema.plugin(uniqueValidator, { message: '{PATH} need to be unique' })
+
+ProductSchema.plugin(mongoosePaginate);
+module.exports = mongoose.model('Product', ProductSchema);
