268417ef08afe3f7f3c6e898c1b8afd667e22ccf
unknown <nathaliecortz@hotmail.com>
2019-06-18 18:40:22 -0500

    Handler errors for required keys

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
 const User = require('../models/User');
 const {
-  requireAuth,
-  requireAdmin,
-  isAuthenticated,
-  isAdmin,
+    requireAuth,
+    requireAdmin,
+    isAuthenticated,
+    isAdmin,
 } = require('../middleware/auth');
 const {
-  createPrivatePropsFilter,
-  getPaginationParamsFromRequest,
-  buildLinkHeader,
+    createPrivatePropsFilter,
+    getPaginationParamsFromRequest,
+    buildLinkHeader,
 } = require('../lib/util');
 
 
@@ -16,24 +16,24 @@ const omitPrivateProps = createPrivatePropsFilter(['password']);
 
 
 const initAdminUser = (app, next) => {
-  const { adminEmail, adminPassword } = app.get('config');
-  if (!adminEmail || !adminPassword) {
-    return next();
-  }
+    const { adminEmail, adminPassword } = app.get('config');
+    if (!adminEmail || !adminPassword) {
+        return next();
+    }
 
-  const adminUser = {
-    email: adminEmail,
-    password: adminPassword,
-    roles: { admin: true },
-  };
+    const adminUser = {
+        email: adminEmail,
+        password: adminPassword,
+        roles: { admin: true },
+    };
 
-  User.findOne({ email: adminEmail })
-    .then(user => (
-      (!user)
-        ? User.create(adminUser, next)
-        : user.save(adminUser, next)
-    ))
-    .catch(next);
+    User.findOne({ email: adminEmail })
+        .then(user => (
+            (!user) ?
+            User.create(adminUser, next) :
+            user.save(adminUser, next)
+        ))
+        .catch(next);
 };
 
 
@@ -65,98 +65,98 @@ const initAdminUser = (app, next) => {
 
 
 module.exports = (app, next) => {
-  /*
-   * Cuando la ruta llamada tenga entre sus parámetros uid, ejecutará la función
-   * que cargará el usuario desde la base de datos, dejándo disponible esa
-   * Información para las rutas que están acá.
-   */
-  app.param('uid', (req, res, next, uid) => {
-    const { auth } = req;
+    /*
+     * Cuando la ruta llamada tenga entre sus parámetros uid, ejecutará la función
+     * que cargará el usuario desde la base de datos, dejándo disponible esa
+     * Información para las rutas que están acá.
+     */
+    app.param('uid', (req, res, next, uid) => {
+        const { auth } = req;
 
-    if (!isAuthenticated(req)) {
-      return next(401);
-    }
-
-    if (auth.user._id === uid || auth.user.email === uid) {
-      Object.assign(req, { user: auth.user });
-      return next();
-    }
-
-    if (!isAdmin(req)) {
-      return next(403);
-    }
-
-    return User.findByIdOrEmail(uid)
-      .then((doc) => {
-        if (!doc) {
-          return next(404);
+        if (!isAuthenticated(req)) {
+            return next(401);
         }
-        Object.assign(req, { user: doc });
-        next();
-      })
-      .catch(next);
-  });
 
+        if (auth.user._id === uid || auth.user.email === uid) {
+            Object.assign(req, { user: auth.user });
+            return next();
+        }
 
-  app.get('/users', requireAdmin, (req, resp) => {
-    User.paginate({}, getPaginationParamsFromRequest(req), (err, results) => {
-      if (err) {
-        return next(err);
-      }
-      const linkHeader = buildLinkHeader(req, results);
-      if (linkHeader) {
-        resp.header({ Link: linkHeader });
-      }
-      resp.json(results.docs.map(omitPrivateProps));
+        if (!isAdmin(req)) {
+            return next(403);
+        }
+
+        return User.findByIdOrEmail(uid)
+            .then((doc) => {
+                if (!doc) {
+                    return next(404);
+                }
+                Object.assign(req, { user: doc });
+                next();
+            })
+            .catch(next);
     });
-  });
 
 
-  app.get('/users/:uid', requireAuth, (req, resp) => resp.json(
-    omitPrivateProps(req.user),
-  ));
+    app.get('/users', requireAdmin, (req, resp) => {
+        User.paginate({}, getPaginationParamsFromRequest(req), (err, results) => {
+            if (err) {
+                return next(err);
+            }
+            const linkHeader = buildLinkHeader(req, results);
+            if (linkHeader) {
+                resp.header({ Link: linkHeader });
+            }
+            resp.json(results.docs.map(omitPrivateProps));
+        });
+    });
 
 
-  app.post('/users', (req, resp, next) => {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      return next(400);
-    }
-
-    User.create({ email, password })
-      .then(doc => resp.json(omitPrivateProps(doc)))
-      .catch(err => (
-        (/duplicate key/.test(err.message))
-          ? next(403)
-          : next(500)
-      ));
-  });
+    app.get('/users/:uid', requireAuth, (req, resp) => resp.json(
+        omitPrivateProps(req.user),
+    ));
 
 
-  app.put('/users/:uid', requireAuth, (req, resp, next) => {
-    if (!Object.keys(req.body || {}).length) {
-      return next(400);
-    }
+    app.post('/users', (req, resp, next) => {
+        const { email, password } = req.body;
 
-    if (req.body.roles && !isAdmin(req)) {
-      return next(403);
-    }
+        if (!email || !password) {
+            return next(400);
+        }
 
-    Object.assign(req.user, req.body);
-
-    req.user.save()
-      .then(doc => resp.json(omitPrivateProps(doc)))
-      .catch(next);
-  });
-
-
-  app.delete('/users/:uid', requireAuth, (req, resp, next) => {
-    req.user.remove()
-      .then(doc => resp.json(omitPrivateProps(doc)))
-      .catch(next);
-  });
+        User.create({ email, password })
+            .then(doc => resp.json(omitPrivateProps(doc)))
+            .catch(err => (
+                (/duplicate key/.test(err.message)) ?
+                next(403) :
+                next(500)
+            ));
+    });
 
 
-  initAdminUser(app, next);
+    app.put('/users/:uid', requireAuth, (req, resp, next) => {
+        if (!Object.keys(req.body || {}).length) {
+            return next(400);
+        }
+
+        if (req.body.roles && !isAdmin(req)) {
+            return next(403);
+        }
+
+        Object.assign(req.user, req.body);
+
+        req.user.save()
+            .then(doc => resp.json(omitPrivateProps(doc)))
+            .catch(next);
+    });
+
+
+    app.delete('/users/:uid', requireAuth, (req, resp, next) => {
+        req.user.remove()
+            .then(doc => resp.json(omitPrivateProps(doc)))
+            .catch(next);
+    });
+
+
+    initAdminUser(app, next);
 };
