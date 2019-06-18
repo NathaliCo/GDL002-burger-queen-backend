6b915fbc43871120d012d1c6adf711c740df7f0a
unknown <nathaliecortz@hotmail.com>
2019-06-18 12:48:41 -0500

    Add get method

---
+++ routes/products.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const Product = require('../models/product');
+
+const app = express();
+
+
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/product', function(req, res) {
+    res.json('Get product');
+});
+
+
+app.post('/product', function(req, res) {
+
+    let body = req.body;
+
+    let product = new Product({
+        name: body.name,
+        price: body.price
+    })
+
+    product.save((err, productDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            product: productDB
+        })
+    });
+});
+
+
+app.put('/product/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+
+
+app.delete('/product', function(req, res) {
+    res.json('Delete product');
+});
+
+module.exports = app;
